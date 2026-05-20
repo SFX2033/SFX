8b23def3a81d38af909ad1f0031f35c1c8fc3333
Alexandre Berger <icecrime03@gmail.com>
2026-05-20 17:20:05 +0200

    mise en place du site  SFX_ALEX

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+// On récupère les données mises à jour par le client
+fetch('/data.json')
+  .then(response => response.json())
+  .then(data => {
+    // On injecte les textes et tarifs dans le HTML
+    document.getElementById('titre-site').innerText = data.site_title;
+    document.getElementById('texte-presentation').innerText = data.presentation_text;
+    document.getElementById('tarif-affichage').innerText = data.price_consultation;
+  });
